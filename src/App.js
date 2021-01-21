9a967e4cbe5f9c5e3593247c5f9447a07facc593
onlycan17 <onlycan17@gmail.com>
2021-01-21 18:34:17 +0900

    20210121 movie study page upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-  add = () => {
-    console.log("add");
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    console.log(movies);
+    this.setState({ movies: movies, isLoading: false });
   };
-  minus = () => {
-    console.log("minus");
-  };
-  render() {
+
+  getLoading = function () {
     return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+      <div class="loader">
+        <span class="loader__text">Loading...</span>
+        <img
+          src="https://devtalk.kakao.com/uploads/default/original/2X/8/8d3426581b592b46157de64b919d4378b504d346.gif"
+          alt="Loading"
+        ></img>
       </div>
+    );
+  };
+
+  getReady = function (movies) {
+    return (
+      <div class="movies">
+        {movies.map((movie) => {
+          return (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />
+          );
+        })}
+      </div>
+    );
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? this.getLoading() : this.getReady(movies)}
+      </section>
     );
   }
 }
